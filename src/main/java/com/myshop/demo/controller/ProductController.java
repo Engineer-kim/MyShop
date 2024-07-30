6f378599474633e3683c34e8059d0da6ec12eb16
Kimhanjin <ddang0103@naver.com>
2024-07-30 15:36:47 +0900

    Top 5 회원 찿기 구현

--- src/main/java/com/myshop/demo/controller/ProductController.java
+++ src/main/java/com/myshop/demo/controller/ProductController.java
@@ -4,6 +4,9 @@ package com.myshop.demo.controller;
 import com.myshop.demo.dto.ProductMyPriceRequestDto;
 import com.myshop.demo.dto.ProductRequestDto;
 import com.myshop.demo.dto.ProductResponseDto;
+import com.myshop.demo.entity.ApiUseTime;
+import com.myshop.demo.entity.User;
+import com.myshop.demo.repository.ApiUseTimeRepository;
 import com.myshop.demo.security.UserDetailsImpl;
 import com.myshop.demo.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +23,40 @@ public class ProductController {
 
     private final ProductService productService;
 
-
+    private final ApiUseTimeRepository apiUseTimeRepository;
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto , @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return  productService.createProduct(requestDto , userDetails.getUser());
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 측정 시작 시간
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 응답 보내기
+            return productService.createProduct(requestDto, userDetails.getUser());
+        } finally {
+            // 측정 종료 시간
+            long endTime = System.currentTimeMillis();
+            // 수행시간 = 종료 시간 - 시작 시간
+            long runTime = endTime - startTime;
+
+            // 로그인 회원 정보
+            User loginUser = userDetails.getUser();
+
+            // API 사용시간 및 DB 에 기록
+            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
+                    .orElse(null);
+            if (apiUseTime == null) {
+                // 로그인 회원의 기록이 없으면
+                apiUseTime = new ApiUseTime(loginUser, runTime);
+            } else {
+                // 로그인 회원의 기록이 이미 있으면
+                apiUseTime.addUseTime(runTime);
+            }
+
+            System.out.println("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
+            apiUseTimeRepository.save(apiUseTime);
+        }
     }
+
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable Long id , @RequestBody ProductMyPriceRequestDto productMyPriceRequestDto){
         return  productService.updateProduct(id,productMyPriceRequestDto);
